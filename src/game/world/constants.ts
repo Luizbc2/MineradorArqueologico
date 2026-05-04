890ad7e89f2e426842c12167668676fd8fb19a11
Luizbc2 <luizotaviomello82@gmail.com>
2026-05-04 19:57:08 -0300

    fix(world): make galactic layer reachable

--- src/game/world/constants.ts
+++ src/game/world/constants.ts
@@ -3,7 +3,7 @@ export const VIEWPORT_WIDTH = 1920;
 export const VIEWPORT_HEIGHT = 1080;
 
 export const WORLD_WIDTH_TILES = 60;
-export const WORLD_HEIGHT_TILES = 500;
+export const WORLD_HEIGHT_TILES = 720;
 
 export const WORLD_WIDTH_PX = WORLD_WIDTH_TILES * TILE_SIZE;
 export const WORLD_HEIGHT_PX = WORLD_HEIGHT_TILES * TILE_SIZE;
